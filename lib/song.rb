82f5a2a214ea274ea582a9ccba539bd051880009
Leticia Molina <letimolina@gmail.com>
2018-06-09 22:12:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,14 +41,14 @@ class Song
     self.all.sort_by {|song| song.name}
   end
   
-  def self.new_from_filename
+  def self.new_from_filename(name)
     split_name = name.sub(/\b.mp3/,'').split(" - ")
     song = self.new_by_name(split_name[1])
     song.artist_name = split_name[0]
     song
   end
   
-  def self.create_from_filename
+  def self.create_from_filename(name)
     split_name = name.sub(/\b.mp3/,'').split(" - ")
     song = self.create_by_name(split_name[1])
     song.artist_name = split_name[0]
